aca0bf7a68d1100cdb039528cad2f623c78daf07
Rajesh Choudhary <rajeshchoudharyt@gmail.com>
2024-11-13 03:41:04 +0530

    Update page.jsx

--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
-"use server";
 import ClientComponent from "./components/ClientComponent";
 import { fetchAccessToken } from "hume";
 
 export default async function Page() {
 	const accessToken = await fetchAccessToken({
-		apiKey: process.env.HUME_API_KEY,
-		secretKey: process.env.HUME_SECRET_KEY,
+		apiKey: String(process.env.HUME_API_KEY),
+		secretKey: String(process.env.HUME_SECRET_KEY),
 	});
 
 	if (!accessToken) throw new Error("Failed to fetch access token");
